f5e061f3dcc4a37d033fd85dce574c9f555a1ecb
Jakub Matyjewicz <niewiem826@gmail.com>
2021-03-23 21:44:07 +0000

    ++

--- src/com/spartaglobal/Jakub/day1/Fibonacci.java
+++ src/com/spartaglobal/Jakub/day1/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
         int n1 = 0;
         int n2 = 1;
 
-        for (int i = 1; i <= count; ++i) {
+        for (int i = 1; i <= count; i++) {
             System.out.print(n1 + " ");
             int sum = n1 + n2;
             n1 = n2;
